9b0a735ba9937baf76c8d7df02ef6d903e80d295
Joao Lucas <joao.lsilva1198@gmail.com>
2019-07-14 22:21:47 -0300

    Algoritmo Simples de Bubble Sort em Javascript

---
+++ bubblesort.js
@@ -0,0 +1,21 @@
+var numeros = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+
+console.log("Antes da ordenação");
+console.log(numeros);
+
+function bubbleSort(numeros) {
+    let tamanho = numeros.length;
+    for (let i = 0; i < tamanho; i++) {
+        for (let j = 0; j < (tamanho - i - 1); j++) {
+            if (numeros[j] > numeros[j + 1]) {
+                let temporaria = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temporaria;
+            }
+        }
+    }
+}
+
+bubbleSort(numeros);
+console.log("Depois da ordenação");
+console.log(numeros);
